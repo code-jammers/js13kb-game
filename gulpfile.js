2c2b18832d5361beaf36364004048cb53288e412
Kasey <kasey-hinton@leavitt.com>
2021-09-12 13:51:18 -0600

    fix build

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var htmlmin = require("gulp-html-minifier");
 var uglifycss = require("gulp-uglifycss");
 var zip = require("gulp-zip");
 var gulpCopy = require("gulp-copy");
-// var fontmin = require("gulp-fontmin");
 var del = require("del");
 const size = require("gulp-size");
 
@@ -13,12 +12,6 @@ task("copy-images", function () {
     .pipe(gulpCopy("dist"))
     .pipe(dest("assets/images"));
 });
-
-// task("minify-fonts", function () {
-//   return src("assets/fonts/*.ttf")
-//     .pipe(fontmin({ text: "{}@#" }))
-//     .pipe(dest("dist/assets/fonts"));
-// });
 
 task("minify-html", function () {
   return src("index.html")
@@ -38,7 +31,7 @@ task("minify-html", function () {
 });
 
 task("minify-js", function () {
-  return src(["index.js", "assets/**/*.js"]).pipe(uglify()).pipe(dest("dist/"));
+  return src(["index.js"]).pipe(uglify()).pipe(dest("dist/"));
 });
 
 task("minify-js-assets-scripts", function () {
@@ -87,15 +80,6 @@ task("clean", function () {
   return del(["dist/**", "!dist"]);
 });
 
-// task("clean-extra-fonts", function () {
-//   return del([
-//     "dist/assets/fonts/PaletteMosaic-Regular.css",
-//     "dist/assets/fonts/PaletteMosaic-Regular.eot",
-//     "dist/assets/fonts/PaletteMosaic-Regular.svg",
-//     "dist/assets/fonts/PaletteMosaic-Regular.woff",
-//   ]);
-// });
-
 task(
   "default",
   series(
@@ -106,9 +90,7 @@ task(
     "minify-web-components",
     "minify-css",
     "minify-component-css",
-    // "minify-fonts",
-    "copy-images",
-    // "clean-extra-fonts"
+    "copy-images"
   )
 );
 task("zip", series("zip"));
